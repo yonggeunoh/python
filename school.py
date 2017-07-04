3bdac3bec7fe12b6d2e4f99dd623aa69212ae655
nakariakov <sergei5@hotmail.com>
2017-07-05 00:24:31 +0900

    Update school.py

--- school.py
+++ school.py
@@ -20,10 +20,20 @@ kndDf = kndDf.drop(2,axis=0)
 
 
 from selenium import webdriver
-
+from selenium.common.exceptions import NoSuchElementException
+from bs4 import BeautifulSoup
 driver = webdriver.Chrome("C:\\app\\webdriver\\chromedriver")
 driver.implicitly_wait(3)
 driver.get('http://www.schoolinfo.go.kr')
+
 driver.find_element_by_id("SEARCH_KEYWORD").send_keys("봉담고등학교 학교회계 예·결산서")
 driver.find_element_by_xpath("//button[@title='검색하기']").click()
-driver.find_element_by_xpath(".//input[@id='ExcelData'][@name='ExcelData'][@type='hidden']").text()
+#try:
+driver.switch_to_frame(driver.find_element_by_xpath("//iframe[@id='pneipp_39']"))
+
+excel1 = driver.find_element_by_xpath("//input[@id='ExcelData']").get_attribute("value")
+excel2 = driver.find_element_by_xpath("//input[@id='ExcelData2']").get_attribute("value")
+
+
+#except NoSuchElementException:
+#    assert 0, "can't find input with XYZ itemcode"
