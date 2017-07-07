20de276839b0d34fdefca74c3b2efe2766bc9b5e
nakariakov <sergei5@hotmail.com>
2017-07-07 21:53:09 +0900

    Update school.py

--- school.py
+++ school.py
@@ -34,15 +34,15 @@ from selenium.common.exceptions import StaleElementReferenceException
 
 def getParentPaid(args):
 
-    #grade = { 'H':'HighSchool'  ,'E':'ElementarySchool' ,'M':'MiddleSchool'}
+#    grade = { 'H':'HighSchool'  }
     grade = {'E':'ElementarySchool' ,'M':'MiddleSchool', 'H':'HighSchool' }
     schoolDf = None
     accountNatinalDf = None
     accountPrivateDf = None
     pathFile = '.\\file\\'
+    pathWebDriver = '.\\webdriver\\'
     debugStr = 'ParentPaid Start !!'
-    
-    
+        
     # 국공립학교 계정
     accountNatinalDf = DataFrame(columns=('학교명','구분', '공시년월','학부모부담수입','등록금'
                                 ,'학교운영지원비', '수익자부담수입','급식비'
@@ -57,7 +57,20 @@ def getParentPaid(args):
                                          ,'기숙사비' ,'기타수익자부담수입' ,'누리과정비' 
                                          ,'교복구입비','운동부운영비'), index=None  )
 
+
+    debugStr = '웹드라이버 선택(팬텀은 화면표시 없음)'
+    if args.browser == 'c':
+        driver = webdriver.Chrome(pathWebDriver + "chromedriver")
+    elif args.browser == 'p':
+        driver = webdriver.PhantomJS(pathWebDriver + "phantomjs-2.1.1-windows\\bin\\phantomjs")
+    elif args.browser == 'i':
+        driver = webdriver.Ie(pathWebDriver + "IEDriverServer_win32_2.45.0")
+    else:
+        driver = webdriver.Chrome(pathWebDriver + "chromedriver")
+
+    debugStr = '학교등급 선택'
     for g in grade:
+        debugStr = '학교정보 읽어오기'
         if g == 'E':
             debugStr = grade.get(g)
             schoolDf = pd.read_excel(pathFile  + "2-2) 초등학교 현황.xlsx")
@@ -67,49 +80,47 @@ def getParentPaid(args):
         elif g == 'H':
             debugStr = grade.get(g)
             schoolDf = pd.read_excel(pathFile  + "2-4) 고등학교 현황.xlsx")
-        elif g == 'E':
-            debugStr = grade.get(g)
-            schoolDf = pd.read_excel(pathFile  + "2-2) 초등학교 현황.xlsx")
         else:
             pass
-        
+
         schoolDf.columns = schoolDf.iloc[2,0:]
         schoolDf = schoolDf.drop(0,axis=0)
         schoolDf = schoolDf.drop(1,axis=0)
         schoolDf = schoolDf.drop(2,axis=0)
-    
-    
 
-        debugStr = '웹드라이버 선택(팬텀은 화면표시 없음)'
-        if args.browser == 'c':
-            driver = webdriver.Chrome("..\\webdriver\\chromedriver")
-        elif args.browser == 'p':
-            driver = webdriver.PhantomJS("..\\webdriver\\phantomjs-2.1.1-windows\\bin\\phantomjs")
-        elif args.browser == 'i':
-            driver = webdriver.Ie("..\\webdriver\\IEDriverServer_win32_2.45.0")
-        else:
-            driver = webdriver.Chrome("..\\webdriver\\chromedriver")
-        
-        
-        
-        debugStr = '변수 초기화'
-        countNational = 0
-        countPrivate = 0
-        schooltype='thead'
-        
-        
-        for schoolName in schoolDf['학교명'].head(12):
+        debugStr = '학교 루프 시작'
+
+#        for idx, row in schoolDf.ix[:5].iterrows():
+        for idx, row in schoolDf.iterrows():
+
+            schoolName = row['학교명'].strip()
+            schoolURL = row['홈페이지'].strip()
+            
             debugStr = '웹 호출'
             driver.implicitly_wait(5)
             driver.get('http://www.schoolinfo.go.kr')
+            debugStr = '변수 초기화'
+            countNational = 0
+            countPrivate = 0
+            schooltype = 'thead'
         
             try:
                 driver.find_element_by_id("SEARCH_KEYWORD").send_keys("")
                 driver.find_element_by_id("SEARCH_KEYWORD").send_keys(schoolName)
                 driver.find_element_by_xpath("//button[@title='검색하기']").click()
                 
-                debugStr = '학교검색 후 결과선택'
-                driver.find_element_by_link_text(schoolName).click()
+                debugStr = '학교검색결과 더보기버튼'
+                while 1:
+                    try:
+                        driver.find_element_by_id("btnMore")
+                    except NoSuchElementException:
+                        break
+                    driver.find_element_by_id("btnMore").click()
+
+                debugStr = '학교검색결과 색인'
+                
+#                driver.find_element_by_xpath("//h1[@class='School_Name']/a[contains(@href,"+schoolName+")]").click()
+                driver.find_element_by_xpath("//a[@href='"+schoolURL+"']/parent::span/parent::li/parent::ul/parent::article/h1[@class='School_Name']/a").click()
                 
                 debugStr = '국공사립 구분확인'
                 establish = driver.find_element_by_xpath("//ul[@class='School_Data']/li[2]").text[5:]
