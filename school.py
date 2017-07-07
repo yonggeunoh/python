8c83b89f719d115a8f818623e08d2534dc54e96b
nakariakov <sergei5@hotmail.com>
2017-07-08 01:37:47 +0900

    Update school.py

--- school.py
+++ school.py
@@ -4,7 +4,7 @@ Created on Thu Jul  6 13:40:56 2017
 @author: 1310615
 """
 
-import argparse
+import argparse, textwrap
 import pandas as pd
 from pandas import DataFrame
 from selenium import webdriver
@@ -20,7 +20,7 @@ from selenium.common.exceptions import StaleElementReferenceException
 #    #생성할때 value 값을 입력 받는다.
 #    def __init__(self, value):
 #        self.value = value
-#     
+#
 #    #생성할때 받은 value 값을 확인 한다.
 #    def __str__(self):
 #        return self.value
@@ -34,82 +34,102 @@ from selenium.common.exceptions import StaleElementReferenceException
 
 def getParentPaid(args):
 
-#    grade = { 'H':'HighSchool'  }
-    grade = {'E':'ElementarySchool' ,'M':'MiddleSchool', 'H':'HighSchool' }
+    grade = {'E':'ElementarySchool' ,'M':'MiddleSchool'    , 'H':'HighSchool'
+            ,'U':'University'       ,'G':'GraduateSchool'
+            ,'K':'Kindergarden'     ,'S':'SpecialSchool'   ,'O':'Others' }
     schoolDf = None
-    accountNatinalDf = None
-    accountPrivateDf = None
+    acctPblcDf = None
+    acctPrvtDf = None
+    driver = None
     pathFile = '.\\file\\'
     pathWebDriver = '.\\webdriver\\'
-    debugStr = 'ParentPaid Start !!'
-        
+    debugLog = 'ParentPaid Start !!'
+
     # 국공립학교 계정
-    accountNatinalDf = DataFrame(columns=('학교명','구분', '공시년월','학부모부담수입','등록금'
-                                ,'학교운영지원비', '수익자부담수입','급식비'
-                                ,'방과후학교활동비', '현장체험학습비','청소년단체활동비'
-                                ,'졸업앨범대금', '교과서대금','기숙사비','기타수익자부담수입'
-                                ,'누리과정비','교복구입비','운동부운영비'), index=None  )
+    acctPblcDf = DataFrame(columns=('학교명','구분', '공시년월','학부모부담수입','등록금'
+                                   ,'학교운영지원비', '수익자부담수입','급식비'
+                                   ,'방과후학교활동비', '현장체험학습비','청소년단체활동비'
+                                   ,'졸업앨범대금', '교과서대금','기숙사비','기타수익자부담수입'
+                                   ,'누리과정비','교복구입비','운동부운영비'), index=None  )
     # 사립학교 계정
-    accountPrivateDf = DataFrame(columns=('학교명','구분', '공시년월','학부모부담수입' ,'등록금'
-                                         ,'입학금' ,'수업료' ,'학교운영지원비' ,'수익자부담수입' 
-                                         ,'급식비' ,'방과후학교활동비' ,'현장체험학습비' 
-                                         ,'청소년단체활동비' ,'졸업앨범대금' ,'교과서대금'
-                                         ,'기숙사비' ,'기타수익자부담수입' ,'누리과정비' 
-                                         ,'교복구입비','운동부운영비'), index=None  )
+    acctPrvtDf = DataFrame(columns=('학교명','구분', '공시년월','학부모부담수입' ,'등록금'
+                                   ,'입학금' ,'수업료' ,'학교운영지원비' ,'수익자부담수입'
+                                   ,'급식비' ,'방과후학교활동비' ,'현장체험학습비'
+                                   ,'청소년단체활동비' ,'졸업앨범대금' ,'교과서대금'
+                                   ,'기숙사비' ,'기타수익자부담수입' ,'누리과정비'
+                                   ,'교복구입비','운동부운영비'), index=None  )
 
 
-    debugStr = '웹드라이버 선택(팬텀은 화면표시 없음)'
-    if args.browser == 'c':
+    debugLog = '웹드라이버 선택(팬텀은 화면표시 없음)'
+    if args.browser == 'C':
         driver = webdriver.Chrome(pathWebDriver + "chromedriver")
-    elif args.browser == 'p':
+    elif args.browser == 'P':
         driver = webdriver.PhantomJS(pathWebDriver + "phantomjs-2.1.1-windows\\bin\\phantomjs")
-    elif args.browser == 'i':
+    elif args.browser == 'I':
         driver = webdriver.Ie(pathWebDriver + "IEDriverServer_win32_2.45.0")
     else:
         driver = webdriver.Chrome(pathWebDriver + "chromedriver")
 
-    debugStr = '학교등급 선택'
+    debugLog = '학교등급 선택'
     for g in grade:
-        debugStr = '학교정보 읽어오기'
+#    for g in ('E','M','H'):
+        debugLog = '학교정보 읽어오기'
         if g == 'E':
-            debugStr = grade.get(g)
             schoolDf = pd.read_excel(pathFile  + "2-2) 초등학교 현황.xlsx")
+            schoolDf.columns = schoolDf.iloc[2,0:]
+            schoolDf = schoolDf.drop([0,1,2],axis=0)
         elif g == 'M':
-            debugStr = grade.get(g)
             schoolDf = pd.read_excel(pathFile  + "2-3) 중학교 현황.xlsx")
+            schoolDf.columns = schoolDf.iloc[2,0:]
+            schoolDf = schoolDf.drop([0,1,2],axis=0)
         elif g == 'H':
-            debugStr = grade.get(g)
             schoolDf = pd.read_excel(pathFile  + "2-4) 고등학교 현황.xlsx")
+            schoolDf.columns = schoolDf.iloc[2,0:]
+            schoolDf = schoolDf.drop([0,1,2],axis=0)
+        elif g == 'S':
+            schoolDf = pd.read_excel(pathFile  + "2-5) 특수학교 및 기타학교 현황.xlsx", sheetname ="특수학교")
+            schoolDf.columns = schoolDf.iloc[2,0:]
+            schoolDf = schoolDf.drop([0,1,2],axis=0)
+        elif g == 'O':
+            schoolDf = pd.read_excel(pathFile  + "2-5) 특수학교 및 기타학교 현황.xlsx", sheetname ="기타학교")
+            schoolDf.columns = schoolDf.iloc[2,0:]
+            schoolDf = schoolDf.drop([0,1,2],axis=0)
+        elif g == 'U':
+            schoolDf = pd.read_excel(pathFile  + "1-1) 대학 현황.xlsx")
+            schoolDf.columns = schoolDf.iloc[4,0:]
+            schoolDf.rename(columns={"학교명(국문)":"학교명"}, inplace = True)
+            schoolDf = schoolDf.drop([0,1,2,3,4],axis=0)
+        elif g == 'G':
+            schoolDf = pd.read_excel(pathFile  + "1-2) 대학원 현황.xlsx")
+            schoolDf.columns = schoolDf.iloc[4,0:]
+            schoolDf.rename(columns={"학교명(국문)":"학교명"}, inplace = True)
+            schoolDf = schoolDf.drop([0,1,2,3,4],axis=0)
         else:
             pass
 
-        schoolDf.columns = schoolDf.iloc[2,0:]
-        schoolDf = schoolDf.drop(0,axis=0)
-        schoolDf = schoolDf.drop(1,axis=0)
-        schoolDf = schoolDf.drop(2,axis=0)
 
-        debugStr = '학교 루프 시작'
+        debugLog = '학교 루프 시작'
 
-#        for idx, row in schoolDf.ix[:5].iterrows():
+        countPublic = 0
+        countPrivate = 0
+#        for idx, row in schoolDf.iloc[:5].iterrows():
         for idx, row in schoolDf.iterrows():
 
             schoolName = row['학교명'].strip()
             schoolURL = row['홈페이지'].strip()
-            
-            debugStr = '웹 호출'
+
+            debugLog = '웹 호출'
             driver.implicitly_wait(5)
             driver.get('http://www.schoolinfo.go.kr')
-            debugStr = '변수 초기화'
-            countNational = 0
-            countPrivate = 0
+            debugLog = '변수 초기화'
             schooltype = 'thead'
-        
+
             try:
                 driver.find_element_by_id("SEARCH_KEYWORD").send_keys("")
                 driver.find_element_by_id("SEARCH_KEYWORD").send_keys(schoolName)
                 driver.find_element_by_xpath("//button[@title='검색하기']").click()
-                
-                debugStr = '학교검색결과 더보기버튼'
+
+                debugLog = '학교검색결과 더보기버튼'
                 while 1:
                     try:
                         driver.find_element_by_id("btnMore")
@@ -117,84 +137,82 @@ def getParentPaid(args):
                         break
                     driver.find_element_by_id("btnMore").click()
 
-                debugStr = '학교검색결과 색인'
-                
-#                driver.find_element_by_xpath("//h1[@class='School_Name']/a[contains(@href,"+schoolName+")]").click()
+                debugLog = '학교검색결과 색인'
                 driver.find_element_by_xpath("//a[@href='"+schoolURL+"']/parent::span/parent::li/parent::ul/parent::article/h1[@class='School_Name']/a").click()
-                
-                debugStr = '국공사립 구분확인'
+
+                debugLog = '국공사립 구분확인'
                 establish = driver.find_element_by_xpath("//ul[@class='School_Data']/li[2]").text[5:]
-                
-                debugStr = '상세정보 클릭'
+
+                debugLog = '상세정보 클릭'
                 driver.find_element_by_link_text('상세정보').click()
-            
-                debugStr = '예결산서 클릭'
+
+                debugLog = '예결산서 클릭'
                 if establish in ('국립','공립'):
                     schooltype = 'thead'
-                    colNo01= '22'
-                    colNo02= '23'
-                    colNo03= '24'
-                    colNo04= '25'
-                    colNo05= '26'
-                    colNo06= '27'
-                    colNo07= '28'
-                    colNo08= '29'
-                    colNo09= '30'
-                    colNo10= '31'
-                    colNo11= '32'
-                    colNo12= '33'
-                    colNo13= '34'
-                    colNo14= '35'
-                    colNo15= '36'
+                    colNo01 = '22'
+                    colNo02 = '23'
+                    colNo03 = '24'
+                    colNo04 = '25'
+                    colNo05 = '26'
+                    colNo06 = '27'
+                    colNo07 = '28'
+                    colNo08 = '29'
+                    colNo09 = '30'
+                    colNo10 = '31'
+                    colNo11 = '32'
+                    colNo12 = '33'
+                    colNo13 = '34'
+                    colNo14 = '35'
+                    colNo15 = '36'
                     driver.find_element_by_link_text('학교회계 예·결산서').click()
-                    
+
                 elif establish == '사립':
                     schooltype = 'tbody'
-                    colNo01= '23'
-                    colNo02= '24'
-                    colNo03= '25'
-                    colNo04= '26'
-                    colNo05= '27'
-                    colNo06= '28'
-                    colNo07= '29'
-                    colNo08= '30'
-                    colNo09= '31'
-                    colNo10= '32'
-                    colNo11= '33'
-                    colNo12= '34'
-                    colNo13= '35'
-                    colNo14= '36'
-                    colNo15= '37'
-                    colNo16= '38'
-                    colNo17= '39'
+                    colNo01 = '23'
+                    colNo02 = '24'
+                    colNo03 = '25'
+                    colNo04 = '26'
+                    colNo05 = '27'
+                    colNo06 = '28'
+                    colNo07 = '29'
+                    colNo08 = '30'
+                    colNo09 = '31'
+                    colNo10 = '32'
+                    colNo11 = '33'
+                    colNo12 = '34'
+                    colNo13 = '35'
+                    colNo14 = '36'
+                    colNo15 = '37'
+                    colNo16 = '38'
+                    colNo17 = '39'
                     driver.find_element_by_link_text('사립학교 교비회계 예·결산서').click()
-                    
+
                 else:
                     pass
-        #            raise_exception(schoolName + " pass !!!")
-            
-                debugStr = 'iframe 해제'
+                    # raise_exception(schoolName + " pass !!!")
+
+                debugLog = 'iframe 해제'
                 driver.switch_to_frame(driver.find_element_by_xpath("//iframe"))
-        
-                debugStr = '자세히보기 클릭'
+
+                debugLog = '자세히보기 클릭'
                 driver.find_element_by_id("btnDetail").click()
-        
-                debugStr = '공시년월 목록 선택'
+
+                debugLog = '공시년월 목록 선택'
                 select = Select(driver.find_element_by_id('select_trans_dt'))
-                
-                debugStr = '공시년월별 자세히 보기 루프'
+
+                debugLog = '공시년월별 자세히 보기 루프'
                 for index in range(len(select.options)):
-                    debugStr = '공시년월별 자세히 보기 루프 select_by_index'
+                    debugLog = '공시년월별 자세히 보기 루프 select_by_index'
                     select = Select(driver.find_element_by_id('select_trans_dt'))
                     select.select_by_index(index)
-        
-                    debugStr = '공시년월별 자세히 보기 루프 existDetail'
+
+                    debugLog = '공시년월별 자세히 보기 루프 existDetail'
                     existDetail = driver.find_element_by_xpath("//div[@id='btnDetail']/a")
                     if existDetail.is_displayed():
-                        debugStr = '공시년월별 자세히 보기 루프 existDetail.click()'
+                        debugLog = '공시년월별 자세히 보기 루프 existDetail.click()'
                         existDetail.click()
-                    
-                    debugStr = '공시년월별 자세히 보기 루프 cell'
+
+                    debugLog = '공시년월별 자세히 보기 루프 cell'
                     cell = []
                     cell.append(schoolName)
                     cell.append(establish)
@@ -217,34 +235,43 @@ def getParentPaid(args):
                     if establish == '사립':
                         cell.append(driver.find_element_by_xpath("//table[@class='TableType1']/"+schooltype+"/tr["+ colNo16 +"]/td").text)
                         cell.append(driver.find_element_by_xpath("//table[@class='TableType1']/"+schooltype+"/tr["+ colNo17 +"]/td").text)
-                    
-                            
-                    # 파일 쓰기 
+
+
+                    # 파일 쓰기
                     if establish in ('국립','공립'):
-                        accountNatinalDf.loc[countNational] = cell
-                        countNational += 1
+                        acctPblcDf.loc[countPublic] = cell
+                        countPublic += 1
                     elif establish == '사립':
-                        accountPrivateDf.loc[countPrivate] = cell
+                        acctPrvtDf.loc[countPrivate] = cell
                         countPrivate += 1
-                    
+
             except NoSuchElementException:
-                print(schoolName + " NoSuchElementException !!!" + debugStr)
+                print(schoolName + " "+ debugLog + "실패 NoSuchElementException !!!" )
                 pass
             except StaleElementReferenceException:
-                print(schoolName + " StaleElementReferenceException !!!" + debugStr)
+                print(schoolName + " "+ debugLog + "실패 StaleElementReferenceException !!!" )
                 pass
-            
-            
-        
-        
-        # 파일 저장 
-        accountNatinalDf.to_excel('.\\'+grade.get(g)+'National.xlsx',  header=True, index=True)
-        accountPrivateDf.to_excel('.\\'+grade.get(g)+'Private.xlsx',  header=True, index=True)
-        
 
+
+        #End Loop schoolDf
+        # 파일 저장
+        acctPblcDf.to_excel('.\\'+grade.get(g)+'Public.xlsx',  header=True, index=True)
+        acctPrvtDf.to_excel('.\\'+grade.get(g)+'Private.xlsx',  header=True, index=True)
+
+    # End Loop Grade
+    driver.close()
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('-b', '--browser', choices=['i', 'c', 'p'], default='c', help="Select Brower. \n i=IE, c=Chrome, p=PhantomJS")
+    parser.add_argument('-b', '--browser', choices=['I', 'C', 'P'], default='C'
+                      , help=textwrap.dedent('''\
+                        Select Brower.
+                        I=IE, C=Chrome, P=PhantomJS '''))
+    parser.add_argument('-g', '--grade', choices=['E', 'M', 'H', 'U', 'G', 'S', 'O'], default='EMHUGSO'
+                      , help=textwrap.dedent('''\
+                        Select Grade.
+                        E = Elementary School, M = Middle School, H = High School
+                      , U = University, G = Graduate School
+                      , S = SpecialSchool, O = Others '''))
     args, unparsed = parser.parse_known_args()
     getParentPaid(args)
